8592e0b2d3de74fa46361e328c3f62a70f405cd0
darkwizard <wizardrad@gmail.com>
2025-10-21 22:50:26 +0700

    add read_and_filter_files func

---
+++ read_data.py
@@ -0,0 +1,29 @@
+def read_and_filter_files(file_paths: list[str]) -> tuple[list[int], list[float], list[str]]:
+    integers = []
+    floats = []
+    strings = []
+
+    for file_path in file_paths:
+        try:
+            with open(file_path, mode="r", encoding="utf-8") as file:
+                print(f"File being processed: {file_path}")
+                for line in file:
+                    processed_line = line.strip()
+                    if not processed_line:
+                        continue
+
+                    try:
+                        integers.append(int(processed_line))
+                    except ValueError:
+                        try:
+                            floats.append(float(processed_line))
+                        except ValueError:
+                            strings.append(line.rstrip("\n"))
+
+        except FileNotFoundError:
+            print(f"Error. File {file_path} not found. Skipping.")
+
+    return integers, floats, strings
+
+
+print(read_and_filter_files(["input1.txt", "input2.txt"]))
